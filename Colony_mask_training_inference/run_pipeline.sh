693a1106aef9a8dd7b9e5e2309bdb4ed795ad5f5
Daniel Saelid <daniel.saelid@alleninstitute.org>
2025-11-20 15:44:08 -0600

    quote

--- Colony_mask_training_inference/run_pipeline.sh
+++ Colony_mask_training_inference/run_pipeline.sh
@@ -16,8 +16,8 @@ echo "count,movie_path,bf_channel" > $1/runtime_data/predict.csv
 echo "0,$2,0" >> $1/runtime_data/predict.csv
 
 export CYTODL_CONFIG_PATH=$PWD/configs
-python -m cyto_dl.eval experiment=im2im/eval_scale1.yaml paths.data_dir=$1/runtime_data paths.log_dir=$1/runtime_data/cyto_dl_logs
-python -m cyto_dl.eval experiment=im2im/eval_scale2.yaml paths.data_dir=$1/runtime_data paths.log_dir=$1/runtime_data/cyto_dl_logs
-python -m cyto_dl.eval experiment=im2im/eval_scale3.yaml paths.data_dir=$1/runtime_data paths.log_dir=$1/runtime_data/cyto_dl_logs
+python -m cyto_dl.eval experiment=im2im/eval_scale1.yaml paths.data_dir="$1/runtime_data" paths.log_dir="$1/runtime_data/cyto_dl_logs"
+python -m cyto_dl.eval experiment=im2im/eval_scale2.yaml paths.data_dir="$1/runtime_data" paths.log_dir="$1/runtime_data/cyto_dl_logs"
+python -m cyto_dl.eval experiment=im2im/eval_scale3.yaml paths.data_dir="$1/runtime_data" paths.log_dir="$1/runtime_data/cyto_dl_logs"
 
 python ColonyMask_merging_thresholding_rearranging.py --outputdir=$1
