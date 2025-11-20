b0ac5b0b90f5cd951b353faea8e921a8e9659dff
Daniel Saelid <daniel.saelid@hpc.corp.alleninstitute.org>
2025-11-20 14:09:46 -0800

    set home env var

--- Colony_mask_training_inference/run_pipeline.sh
+++ Colony_mask_training_inference/run_pipeline.sh
@@ -16,6 +16,7 @@ echo "count,movie_path,bf_channel" > $1/runtime_data/predict.csv
 echo "0,$2,0" >> $1/runtime_data/predict.csv
 
 export CYTODL_CONFIG_PATH=$PWD/configs
+export HOME=/home/daniel.saelid
 python -m cyto_dl.eval experiment=im2im/eval_scale1.yaml paths.data_dir="$1/runtime_data" paths.log_dir="$1/runtime_data/cyto_dl_logs"
 python -m cyto_dl.eval experiment=im2im/eval_scale2.yaml paths.data_dir="$1/runtime_data" paths.log_dir="$1/runtime_data/cyto_dl_logs"
 python -m cyto_dl.eval experiment=im2im/eval_scale3.yaml paths.data_dir="$1/runtime_data" paths.log_dir="$1/runtime_data/cyto_dl_logs"
