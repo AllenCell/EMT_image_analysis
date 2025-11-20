8f3799e9ebc7b884f248864b3e10a8b0df0a4cb6
Daniel Saelid <daniel.saelid@alleninstitute.org>
2025-11-20 11:30:40 -0600

    modify config

---
+++ Colony_mask_training_inference/run_pipeline.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source .venv/bin/activate
+# first arg is the output directory, second arg is the source file path
+mkdir -p $1
+mkdir $1/runtime_data
+mkdir $1/output
+
+# generate csv to use as input
+echo "count,movie_path,bf_channel\n0,$2,0" > $1/runtime_data/predict.csv
+
+export CYTODL_CONFIG_PATH=$PWD/configs
+python -m cyto_dl.eval experiment=im2im/eval_scale1.yaml paths.data_dir=$1/runtime_data
+python -m cyto_dl.eval experiment=im2im/eval_scale2.yaml paths.data_dir=$1/runtime_data
+python -m cyto_dl.eval experiment=im2im/eval_scale3.yaml paths.data_dir=$1/runtime_data
+
+python ColonyMask_merging_thresholding_rearranging.py --outputdir=$1
