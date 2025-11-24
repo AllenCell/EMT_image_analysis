dc5f4d77676ba3bfdeca0f9ff02fcc2b515a2716
Daniel Saelid <daniel.saelid@alleninstitute.org>
2025-11-24 10:01:34 -0600

    lots of quotes

--- Colony_mask_training_inference/run_pipeline.sh
+++ Colony_mask_training_inference/run_pipeline.sh
@@ -6,19 +6,21 @@
 
 source .venv/bin/activate
 # first arg is the output directory, second arg is the source file path
-mkdir -p $1
-mkdir $1/runtime_data
-mkdir $1/runtime_data/cyto_dl_logs
-mkdir $1/output
+# note that quotes are used extensively to avoid issues with spaces/special chars in file paths
+mkdir -p "$1"
+mkdir "$1/runtime_data"
+mkdir "$1/runtime_data/cyto_dl_logs"
+mkdir "$1/output"
 
 # generate csv to use as input
-echo "count,movie_path,bf_channel" > $1/runtime_data/predict.csv
-echo "0,$2,0" >> $1/runtime_data/predict.csv
+echo "count,movie_path,bf_channel" > "$1/runtime_data/predict.csv"
+echo "0,$2,0" >> "$1/runtime_data/predict.csv"
 
 export CYTODL_CONFIG_PATH=$PWD/configs
 export HOME=/home/daniel.saelid
-python -m cyto_dl.eval experiment=im2im/eval_scale1.yaml paths.data_dir="$1/runtime_data" paths.log_dir="$1/runtime_data/cyto_dl_logs"
-python -m cyto_dl.eval experiment=im2im/eval_scale2.yaml paths.data_dir="$1/runtime_data" paths.log_dir="$1/runtime_data/cyto_dl_logs"
-python -m cyto_dl.eval experiment=im2im/eval_scale3.yaml paths.data_dir="$1/runtime_data" paths.log_dir="$1/runtime_data/cyto_dl_logs"
+# explain if works
+python -m cyto_dl.eval experiment=im2im/eval_scale1.yaml 'paths.data_dir="'"$1"'/runtime_data"' 'paths.log_dir="'"$1"'/runtime_data/cyto_dl_logs"'
+python -m cyto_dl.eval experiment=im2im/eval_scale2.yaml 'paths.data_dir="'"$1"'/runtime_data"' 'paths.log_dir="'"$1"'/runtime_data/cyto_dl_logs"'
+python -m cyto_dl.eval experiment=im2im/eval_scale3.yaml 'paths.data_dir="'"$1"'/runtime_data"' 'paths.log_dir="'"$1"'/runtime_data/cyto_dl_logs"'
 
-python ColonyMask_merging_thresholding_rearranging.py --outputdir=$1
+python ColonyMask_merging_thresholding_rearranging.py --outputdir="$1"
