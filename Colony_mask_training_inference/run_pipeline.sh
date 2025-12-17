b3457574052e3c5a268d75a51e87edf6afe144e3
Daniel Saelid <daniel.saelid@alleninstitute.org>
2025-12-17 19:33:03 +0000

    disable hydra logging to cwd

--- Colony_mask_training_inference/run_pipeline.sh
+++ Colony_mask_training_inference/run_pipeline.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 source .venv/bin/activate
 # first arg is the output directory, second arg is the source file path, third arg is source file name, fourth arg is csv manifest path
@@ -21,8 +22,8 @@ python locking_manifest_writer.py "$4" --filepath "$2" --filename "$3" --parentf
 # in order to pass special characters to a hydra CLI override, you need syntax like 'paths.data_dir="some spaces(!)"'
 # the string (possibly) containing special characters is an arg to this script, and args can't be used within single quotes
 # so, we concatenate by ending the single quote string, inserting a double quote string with the arg, then continuing the single quote string
-python -m cyto_dl.eval experiment=im2im/eval_scale1.yaml 'paths.data_dir="'"$1"'/runtime_data"' 'paths.log_dir="'"$1"'/runtime_data/cyto_dl_logs"'
-python -m cyto_dl.eval experiment=im2im/eval_scale2.yaml 'paths.data_dir="'"$1"'/runtime_data"' 'paths.log_dir="'"$1"'/runtime_data/cyto_dl_logs"'
-python -m cyto_dl.eval experiment=im2im/eval_scale3.yaml 'paths.data_dir="'"$1"'/runtime_data"' 'paths.log_dir="'"$1"'/runtime_data/cyto_dl_logs"'
+python -m cyto_dl.eval hydra/job_logging=disabled experiment=im2im/eval_scale1.yaml 'paths.data_dir="'"$1"'/runtime_data"' 'paths.log_dir="'"$1"'/runtime_data/cyto_dl_logs"'
+python -m cyto_dl.eval hydra/job_logging=disabled experiment=im2im/eval_scale2.yaml 'paths.data_dir="'"$1"'/runtime_data"' 'paths.log_dir="'"$1"'/runtime_data/cyto_dl_logs"'
+python -m cyto_dl.eval hydra/job_logging=disabled experiment=im2im/eval_scale3.yaml 'paths.data_dir="'"$1"'/runtime_data"' 'paths.log_dir="'"$1"'/runtime_data/cyto_dl_logs"'
 
 python ColonyMask_merging_thresholding_rearranging.py --outputdir="$1" --csvmanifest="$4"
