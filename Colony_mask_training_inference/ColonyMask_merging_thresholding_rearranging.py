586278362c9a5c5cd9d5604229ce9204cae47a6b
Daniel Saelid <daniel.saelid@alleninstitute.org>
2026-01-22 21:05:37 +0000

    use bioio writer

--- Colony_mask_training_inference/ColonyMask_merging_thresholding_rearranging.py
+++ Colony_mask_training_inference/ColonyMask_merging_thresholding_rearranging.py
@@ -4,8 +4,8 @@ import numpy as np
 from bioio import BioImage
 from locking_manifest_writer import append_to_csv_manifest
 from pathlib import Path
+from bioio.writers import OmeTiffWriter
 from skimage.filters import threshold_otsu
-from tifffile import imwrite
 
 
 def main():
@@ -35,7 +35,7 @@ def main():
 
         out = bw_all.astype(np.uint8)
         out[out > 0] = 255
-        imwrite(target_file, out)
+        OmeTiffWriter.save(out, target_file)
         if csv_manifest:
             # name of the output directory is the name of the source file being put through ACM
             append_to_csv_manifest(csv_manifest, str(target_file.absolute()), target_file.name, output_dir.name)
